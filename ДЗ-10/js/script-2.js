f07db434953509da3b503d248ce57156262c2a4b
YevheniiSkrypkin <81817746+YevheniiSkrypkin@users.noreply.github.com>
2021-06-23 16:35:56 +0200

    Add files via upload

---
+++ ДЗ-10/js/script-2.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 3, 4, 5];
+let result = 0;
+for(i=0; i < arr.length; i++) {
+    result += arr[i]    
+}
+console.log(result)
+
+console.log('------------------------------------TASK 4 COMPLETED---------------------------------------')
+
+const arr2 = [1, 2, 3, 4, 5];
+let result2 = 0;
+for (r=0; r < arr2.length; r++){
+    result2 += Math.pow(arr[r], 2)
+}
+console.log(result2)
+console.log('------------------------------------TASK 5 COMPLETED---------------------------------------')
